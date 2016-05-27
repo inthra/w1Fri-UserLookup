7a137d43150b5f391308b586b5cc7fad842a52ce
Inthrayuth Mahaphol <zign.int@gmail.com>
2016-05-27 14:05:45 -0700

    Fix for loop error that show in the console log because the loop has extra iteration. To fix these, change <= to <.

--- js/lookup.js
+++ js/lookup.js
@@ -8,8 +8,8 @@ exports.Lookup = function(){
 exports.Lookup.prototype.getRepos = function(username){
   $.get(ghLink + username + '/repos?access_token=' + apiKey).then(function(response){
     var maxLength = response.length;
-    for (var i = 0; i <= maxLength; i++) {
-      $('#gh_repoNameDes').append("<li>Repository name: <span class='boldText'> " + response[i].name + '</span>' + "<br>" + "Description: "+ response[i].description + "</li>");
+    for (var i = 0; i < maxLength; i++) {
+      $('#gh_repoNameDes').append("<li>Repository name:<span class='boldText'> " + response[i].name + '</span><br>' + 'Description:  '+ response[i].description + '</li>');
     }
   }).fail(function(error){
     console.log(error.responseJSON.message);
